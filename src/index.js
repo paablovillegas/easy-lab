3a906aca0566a1960d238d0a05e7fe4c616cfa25
Pablo Villegas <pvillegas241@gmail.com>
2021-01-18 18:22:14 -0600

    Custom form terminado Acepta icono, placeholder, titulo y tipo de dato personalizado

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { FormComponent } from './components/FormComponent';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-ReactDOM.render(<FormComponent />, document.getElementById('root'));
+ReactDOM.render(
+    <FormComponent placeholder="Nombre" icon={faUser} inputType="number"/>, 
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
