354faf1cfe3c6258538ab54325b5314fb70495af
Pablo Villegas <pvillegas241@gmail.com>
2021-01-18 19:10:00 -0600

    Boton agregado y se agrega estilo en tailwind Active state añadido en tailwind

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+import { RegularButton } from './forms/RegularButton'
 import { RegularInput } from './forms/RegularInput'
 import { RoundInput } from './forms/RoundInput'
 
@@ -8,6 +9,7 @@ export const Main = () => {
         <div className="flex flex-col px-2 space-y-6">
             <RegularInput placeholder="Nombre" icon={faUser} inputType="number"/>
             <RoundInput  placeholder="Nombre, correo, teléfono" icon={faSearch}/>
+            <RegularButton title="Actualizar"/>
         </div>
     )
 }
--- src/components/forms/RoundInput.js
+++ src/components/forms/RoundInput.js
@@ -3,19 +3,19 @@ import React from 'react'
 
 export const RoundInput = ({title, placeholder, icon, inputType='text'}) => {
     return (
-        <div>
+        <>
             {
                 title && 
                 <label className="block mt-2 mb-1 text-sm text-gray-300">{title}</label>
             }
             <div className="relative">
-                <FontAwesomeIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-300" icon={icon} />
+                <FontAwesomeIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-300" icon={icon} size="sm"/>
                 <input 
                     type={inputType} 
                     placeholder={placeholder} 
-                    className="bg-gray-50 text-gray-600 placeholder-gray-300 rounded-full shadow-sm ring-1 ring-gray-700 py-2 pl-11 pr-2 w-full transition duration-300 focus:outline-none focus:shadow-md"
+                    className="bg-gray-50 text-gray-500 placeholder-gray-300 rounded-full shadow-sm ring-1 ring-gray-700 py-1.5 pl-11 pr-2 w-full transition duration-300 focus:outline-none focus:shadow-md text-sm"
                 />
             </div>
-        </div>
+        </>
     )
 }
