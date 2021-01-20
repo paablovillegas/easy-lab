5df45a3be451425150d6beaa385167e551c392cc
Pablo Villegas <pvillegas241@gmail.com>
2021-01-20 09:45:37 -0600

    Se agrega opcion menu desplegable en XS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,95 @@
-import { faClipboardList, faHdd, faHospital, faInfoCircle, faMoneyCheck, faSignOutAlt, faUser, faUserMd, faVial } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faClipboardList, faHdd, faHospital, faInfoCircle, faList, faMoneyCheck, faSignOutAlt, faUser, faUserMd, faVial } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import './NavBar.css'
 
 export const NavBar = () => {
     return (
         <div className="bg-gray-700 w-100 h-full flex flex-col overflow-y-hidden">
-            <div className="flex-grow mt-14 flex flex-col space-y-3">
-                <button className="mx-auto lg:ml-2 lg:mr-2 lg:pl-2 lg:text-left px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
-                    <FontAwesomeIcon className="text-yellow-400 lg:hidden xl:inline-block xl:mr-3" icon={faMoneyCheck} />
-                    <h4 className="hidden lg:inline-block text-xl text-yellow-400 font-bold uppercase">Catálogos</h4>
-                </button>
-                <div className="mx-2 sm:mx-0 flex flex-col space-y-1">
-                    <button className="py-1 mx-0.5 rounded transition focus:outline-none active:bg-gray-800 lg:text-left lg:mx-2 lg:px-2">
-                        <FontAwesomeIcon className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" icon={faUser} size="sm" />
-                        <h5 className="hidden lg:inline-block text-md text-gray-400 uppercase">Pacientes</h5>
-                    </button>
-                    <button className="py-1 mx-0.5 rounded transition focus:outline-none active:bg-gray-800 lg:text-left lg:mx-2 lg:px-2">
-                        <FontAwesomeIcon className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" icon={faUserMd}  size="sm" />
-                        <h5 className="hidden lg:inline-block text-md text-gray-400 uppercase">Doctores</h5>
-                    </button>
-                    <button className="py-1 mx-0.5 rounded transition focus:outline-none active:bg-gray-800 lg:text-left lg:mx-2 lg:px-2">
-                        <FontAwesomeIcon className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" icon={faHospital}  size="sm" />
-                        <h5 className="hidden lg:inline-block text-md text-gray-400 uppercase">Instituciones</h5>
-                    </button>
-                    <button className="py-1 mx-0.5 rounded transition focus:outline-none active:bg-gray-800 lg:text-left lg:mx-2 lg:px-2">
-                        <FontAwesomeIcon className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" icon={faVial}  size="sm" />
-                        <h5 className="hidden lg:inline-block text-md text-gray-400 uppercase">Análisis</h5>
-                    </button>
-                </div>
-                <button className="mx-auto lg:ml-2 lg:mr-2 lg:pl-2 lg:text-left px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
-                    <FontAwesomeIcon className="text-yellow-400 lg:hidden xl:inline-block xl:mr-3" icon={faMoneyCheck} />
-                    <h4 className="hidden lg:inline-block text-xl text-yellow-400 font-bold uppercase">Registros</h4>
-                </button>
-                <button className="mx-auto lg:ml-2 lg:mr-2 lg:pl-2 lg:text-left px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
-                    <FontAwesomeIcon className="text-yellow-400 lg:hidden xl:inline-block xl:mr-5" icon={faClipboardList} />
-                    <h4 className="hidden lg:inline-block text-xl text-yellow-400 font-bold uppercase">Reportes</h4>
+            <div className="flex sm:hidden flex-row pr-3">
+                <h3
+                    className="flex-grow m-3 text-gray-200"
+                >
+                    Laboratorio Nombre
+                </h3>
+                <button className="normal p-3 m-2">
+                    <FontAwesomeIcon
+                        className="text-gray-400 lg:hidden xl:inline-block xl:mr-2"
+                        icon={faList} 
+                    />
                 </button>
             </div>
-            <div className="flex-none mb-3 flex flex-col lg:flex-row space-y-1">
-                <button className="mx-auto lg:ml-3 px-3 py-2 rounded transition focus:outline-none active:bg-gray-800">
-                    <FontAwesomeIcon className="text-gray-400" icon={faInfoCircle} />
+            <div className="flex-grow mt-3 sm:mt-6 flex flex-col space-y-3">
+                <button className="primary">
+                    <FontAwesomeIcon 
+                        className="align-baseline text-yellow-400 lg:hidden xl:inline-block xl:mr-3" 
+                        icon={faHdd}
+                    />
+                    <h4>Catálogos</h4>
                 </button>
-                <button className="mx-auto lg:mr-3 px-3 py-2 rounded transition focus:outline-none active:bg-gray-800">
-                    <FontAwesomeIcon className="text-gray-400" icon={faSignOutAlt} />
+                <div className="mx-2 flex flex-col space-y-1">
+                    <button className="normal">
+                        <FontAwesomeIcon 
+                            className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
+                            icon={faUser}
+                            size="sm"
+                        />
+                        <h5>Pacientes</h5>
+                    </button>
+                    <button className="normal">
+                        <FontAwesomeIcon 
+                            className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
+                            icon={faUserMd}
+                            size="sm"
+                        />
+                        <h5>Doctores</h5>
+                    </button>
+                    <button className="normal">
+                        <FontAwesomeIcon 
+                            className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
+                            icon={faHospital}
+                            size="sm"
+                        />
+                        <h5>Instituciones</h5>
+                    </button>
+                    <button className="normal">
+                        <FontAwesomeIcon 
+                            className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
+                            icon={faVial}
+                            size="sm"
+                        />
+                        <h5>Análisis</h5>
+                    </button>
+                </div>
+                <button className="primary">
+                    <FontAwesomeIcon 
+                        className="align-baseline text-yellow-400 lg:hidden xl:inline-block xl:mr-3" 
+                        icon={faMoneyCheck}
+                    />
+                    <h4>Registros</h4>
+                </button>
+                <button className="primary">
+                    <FontAwesomeIcon 
+                        className="align-baseline text-yellow-400 lg:hidden xl:inline-block xl:mr-5" 
+                        icon={faClipboardList}
+                    />
+                    <h4>Reportes</h4>
+                </button>
+            </div>
+            <div className="flex-none mb-3 mt-3 flex flex-col lg:flex-row space-y-1 lg:space-y-0 lg:justify-center lg:space-x-6">
+                <button className="mx-2 lg:ml-3 px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
+                    <FontAwesomeIcon 
+                        className="text-gray-400" 
+                        icon={faInfoCircle}
+                    />
+                    <h5 className="lg:hidden">Información</h5>
+                </button>
+                <button className="mx-2 lg:mr-3 px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
+                    <FontAwesomeIcon 
+                        className="text-gray-400" 
+                        icon={faSignOutAlt}
+                    />
+                    <h5 className="lg:hidden">Cerrar Sesión</h5>
                 </button>
             </div>
         </div>
--- src/components/Prueba.js
+++ src/components/Prueba.js
@@ -4,7 +4,7 @@ import { NavBar } from './NavBar'
 export const Prueba = () => {
     return (
         <div className="flex flex-col sm:flex-row h-screen">
-            <div className="h-14 sm:h-auto sm:w-16 lg:w-1/6">
+            <div className="sm:w-16 lg:w-1/6">
                 <NavBar />
             </div>
             <div className="md:w-1/4 xl:w-1/5 bg-blue-500"></div>
