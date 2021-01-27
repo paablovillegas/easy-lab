8c3817edab97258015c956c73c2912d8c9bab5b0
Pablo Villegas <pvillegas241@gmail.com>
2021-01-26 20:57:16 -0600

    Acomodo nav-bar Completamente probada y ya funicona responsivamente

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,101 +1,102 @@
 import { faClipboardList, faHdd, faHospital, faInfoCircle, faList, faMoneyCheck, faSignOutAlt, faUser, faUserMd, faVial } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 export const NavBar = () => {
+    const [menuState, setMenuState] = useState(false);
+
+    const showHideMenu = () => {
+        setMenuState(state => !state)
+    }
+
     return (
         <div className="bg-gray-700 w-100 sm:min-h-screen flex flex-col overflow-y-hidden">
             <div className="flex sm:hidden flex-row pr-3">
-                <h3
-                    className="flex-grow m-3 text-gray-200"
-                >
+                <h3 className="flex-grow ml-3 my-auto text-gray-200 align-baseline">
                     Laboratorio Nombre
                 </h3>
-                <button className="normal p-3 m-2">
+                <button className="normal p-2 m-2">
                     <FontAwesomeIcon
                         className="text-gray-400 lg:hidden xl:inline-block xl:mr-2"
                         icon={faList} 
+                        onClick={showHideMenu}
                     />
                 </button>
             </div>
-            <div className="flex-grow mt-3 sm:mt-6 flex flex-col space-y-3">
-                <button className="primary">
+            <div className={`flex flex-grow mt-3 sm:mt-6 flex-col space-y-3 ${menuState && 'hidden sm:flex'}`}>
+                <Link className="primary" to="/catalogos">
                     <FontAwesomeIcon 
                         className="align-baseline text-yellow-400 lg:hidden xl:inline-block xl:mr-3" 
                         icon={faHdd}
                     />
                     <h4>Catálogos</h4>
-                </button>
+                </Link>
                 <div className="mx-2 flex flex-col space-y-1">
-                    <button className="normal">
+                    <Link className="normal" to="/">
                         <FontAwesomeIcon 
                             className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
                             icon={faUser}
                             size="sm"
                         />
                         <h5>Pacientes</h5>
-                    </button>
-                    <button className="normal">
-                        <Link to="/catalogos/doctores">
-                            <FontAwesomeIcon 
-                                className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
-                                icon={faUserMd}
-                                size="sm"
-                            />
-                            <h5>Doctores</h5>
-                        </Link>
-                    </button>
-                    <button className="normal">
-                        <Link to="/catalogos/instituciones">
-                            <FontAwesomeIcon 
-                                className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
-                                icon={faHospital}
-                                size="sm"
-                            />
-                            <h5>Instituciones</h5>
-                        </Link>
-                    </button>
-                    <button className="normal">
+                    </Link>
+                    <Link className="normal" to="/catalogos/doctores">
+                        <FontAwesomeIcon 
+                            className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
+                            icon={faUserMd}
+                            size="sm"
+                        />
+                        <h5>Doctores</h5>
+                    </Link>
+                    <Link className="normal" to="/catalogos/instituciones">
+                        <FontAwesomeIcon 
+                            className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
+                            icon={faHospital}
+                            size="sm"
+                        />
+                        <h5>Instituciones</h5>
+                    </Link>
+                    <Link className="normal" to="/">
                         <FontAwesomeIcon 
                             className="text-gray-400 lg:hidden xl:inline-block xl:mr-2" 
                             icon={faVial}
                             size="sm"
                         />
                         <h5>Análisis</h5>
-                    </button>
+                    </Link>
                 </div>
-                <button className="primary">
+                <Link className="primary" to="/">
                     <FontAwesomeIcon 
                         className="align-baseline text-yellow-400 lg:hidden xl:inline-block xl:mr-3" 
                         icon={faMoneyCheck}
                     />
                     <h4>Registros</h4>
-                </button>
-                <button className="primary">
+                </Link>
+                <Link className="primary" to="/">
                     <FontAwesomeIcon 
                         className="align-baseline text-yellow-400 lg:hidden xl:inline-block xl:mr-5" 
                         icon={faClipboardList}
                     />
                     <h4>Reportes</h4>
-                </button>
+                </Link>
             </div>
-            <div className="flex-none mb-3 mt-3 flex flex-col lg:flex-row space-y-1 lg:space-y-0 lg:justify-center lg:space-x-6">
-                <button className="mx-2 lg:ml-3 px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
+            <div className={`flex flex-none my-3 flex-col lg:flex-row space-y-1 lg:space-y-0 lg:justify-between ${menuState && 'hidden sm:flex'}`}>
+                <Link to="/" className="text-center px-3 py-1 mx-2 lg:ml-4 rounded transition focus:outline-none active:bg-gray-800">
                     <FontAwesomeIcon 
                         className="text-gray-400" 
                         icon={faInfoCircle}
                     />
                     <h5 className="lg:hidden">Información</h5>
-                </button>
-                <button className="mx-2 lg:mr-3 px-3 py-1 rounded transition focus:outline-none active:bg-gray-800">
+                </Link>
+                <Link to="/" className="text-center px-3 py-1 mx-2 lg:mr-4 rounded transition focus:outline-none active:bg-gray-800">
                     <FontAwesomeIcon 
                         className="text-gray-400" 
                         icon={faSignOutAlt}
                     />
                     <h5 className="lg:hidden">Cerrar Sesión</h5>
-                </button>
+                </Link>
             </div>
         </div>
     )
--- src/routers/CatalogosRouter.js
+++ src/routers/CatalogosRouter.js
@@ -1,43 +1,17 @@
-import React, { useReducer, useState } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 import { DoctorForm } from '../components/forms/catalogos/DoctorForm';
 import { InstitucionForm } from '../components/forms/catalogos/InstitucionForm';
 import { InicioCatalogo } from '../components/screens/catalogos/_InicioScreen';
-import { SearchBar } from '../components/sub-items/search-bar/SearchBar';
-import { changePaciente, dataReducer } from '../hooks/dataReducer';
-import { pacientesApi } from '../sample/Pacientes';
 
 export const CatalogosRouter = () => {
-    const [barraLateral, setBarraLateral] = useState(true);
-    const [pacientes, dispatchPacientes] = useReducer(dataReducer, []);
-    const [paciente, dispatchPaciente] = useReducer(changePaciente);
-
-    setTimeout(() => {
-        dispatchPacientes({
-            type: 'fetch',
-            payload: pacientesApi
-        })
-    }, 1500);
-
     return (
         <div className="flex flex-1">
-            <div className={
-                barraLateral 
-                    ? 'sm:flex-1 lg:flex-initial lg:w-1/4' 
-                    : 'hidden xl:flex xl:w-1/4'
-            }>
-                <SearchBar
-                    data={pacientes}
-                    selection={dispatchPaciente}
-                />
-            </div>
-            <div className="flex-1">
-                <Switch>
-                    <Route exact path="/catalogos" render={ (props) => <InicioCatalogo {...props} hideBarra={setBarraLateral} titulo='Ejemplo' /> } />
-                    <Route path="/catalogos/instituciones" component={InstitucionForm} />
-                    <Route path="/catalogos/doctores" component={DoctorForm} />
-                </Switch>
-            </div>
+            <Switch>
+                <Route exact path="/catalogos" component={InicioCatalogo} />
+                <Route path="/catalogos/instituciones" component={InstitucionForm} />
+                <Route path="/catalogos/doctores" component={DoctorForm} />
+            </Switch>
         </div>
     )
 }
